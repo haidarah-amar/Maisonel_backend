bcbc2f4a2c82cc5eac00bfa194b7429f8c129c95
Dannykhouli <khoulidani@gmail.com>
2025-12-20 17:01:52 +0300

    fix: remove unused user route and its middleware

--- routes/api.php
+++ routes/api.php
@@ -10,12 +10,6 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Termwind\Components\Raw;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 Route::get('/logout', [UserController::class, 'logout']);
